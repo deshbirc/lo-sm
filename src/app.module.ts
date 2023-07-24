143e790bcb2434e336dea6108875e61e6ded40ba
deshbirc <10251562+deshbirc@users.noreply.github.com>
2023-07-24 16:25:00 +0530

    changing the root path

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      serveRoot: '/client',
+      serveRoot: '/',
       rootPath: join(__dirname, '..', 'client'),
     }),
   ],
